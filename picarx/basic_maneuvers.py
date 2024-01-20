f16a88c999dcb4680ddb3f15347af96f0cf22e38
ryanschofield9 <schofier@oregonstate.edu>
2024-01-20 10:32:52 -0800

    Added three point turn capabilities to basic maneuvers

--- picarx/basic_maneuvers.py
+++ picarx/basic_maneuvers.py
@@ -8,20 +8,15 @@ logging_format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=logging_format, level=logging.INFO,
 datefmt="%H:%M:%S")
 logging.getLogger().setLevel(logging.DEBUG)
+car = Picarx()
 
-if __name__ == "__main__":
-    time.sleep(2)
-    car = Picarx()
-    logging.debug("Starting program")
-    
+def parallel_park():
     car.backward(100, 70)
     time.sleep(2)
     
     car.backward(100, 50)
     time.sleep(1)
-    
-    #car.backward(100, 20)
-    #time.sleep (1)
+
     car.backward(100,0)
     time.sleep(1)
 
@@ -34,4 +29,27 @@ if __name__ == "__main__":
     time.sleep(1)
     car.stop()
 
+def three_turn():
+    car.forward(100,100)
+    time.sleep(1)
+
+    car.stop()
+    time.sleep(0.5)
+
+    car.backward(100, 45)
+    time.sleep(1)
+
+    car.stop()
+    time.sleep(0.5)
+
+    car.forward(100,10)
+    time.sleep(1)
+
+    car.forward(100,0)
+    time.sleep(2)
+
+if __name__ == "__main__":
+    time.sleep(2)
+    logging.debug("Starting program")
+    
     logging.debug("Finished program")
