1e739c89ecb7fc262b0bc901428f475fac1ce379
ryanschofield9 <schofier@oregonstate.edu>
2024-01-29 14:29:03 -0800

    updating follow line function

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -427,7 +427,7 @@ if __name__ == "__main__":
     #time.sleep(1)
     #follow_line()
     start_time = time.time()
-    run_time = 15
+    run_time = 3
     px.set_dir_servo_angle(0)
     #px.forward(30)
     #time.sleep(3)
