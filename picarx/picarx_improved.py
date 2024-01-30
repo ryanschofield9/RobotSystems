31a6d8ab1240d44d3ec35abad18ef6eae56c5d8b
ryanschofield9 <schofier@oregonstate.edu>
2024-01-30 13:10:08 -0800

    follow line

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -416,7 +416,7 @@ def follow_line():
 def follow_line_cam():
     angle = controller_cam.control_car()
     px.set_dir_servo_angle(angle)
-    px.forward(25)
+    px.forward(40)
     time.sleep(0.1)
     
     
