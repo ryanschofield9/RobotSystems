5e18d9fab85b7e27816d3fdc587e033282bcb1b5
ryanschofield9 <schofier@oregonstate.edu>
2024-01-30 09:51:05 -0800

    adding camera driving.py

--- picarx/camera_driving.py
+++ picarx/camera_driving.py
@@ -57,12 +57,21 @@ class Interpreter():
 
 # help from https://docs.opencv.org/3.4/d6/d10/tutorial_py_houghlines.html for the hough line transformation 
     def process (self):
+        '''
         edge = picTaker.takePicture()
         lines = cv.HoughLinesP(edge, 1, np.pi/180, 100, minLineLength= 100, maxLineGap =10)
         crop = picTaker.lastCrop
         for line in lines: 
             x1,y1,x2,y2 = line[0]
             cv.line(crop, (x1, y1), (x2, y2), (0,255,0),2)
+        '''
+        img = cv.imread(cv.samples.findFile('sudoku.png'))
+        gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        edges = cv.Canny(gray,50,150,apertureSize = 3)
+        lines = cv.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
+        for line in lines:
+            x1,y1,x2,y2 = line[0]
+            cv.line(img,(x1,y1),(x2,y2),(0,255,0),2)
         plt.imshow(crop,cmap='gray')
         plt.show(block=False)
         plt.pause(3)
