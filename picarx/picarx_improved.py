9a152ffa5c6f73806956de81232884630cd84aa0
ryanschofield9 <schofier@oregonstate.edu>
2024-02-04 12:25:25 -0800

    updating follow line function

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -289,7 +289,7 @@ class Sensor():
 
 class Interpreter():
     def __init__(self, sensitivity_given:float = 0.5, 
-                 polarity_given:int = 0):
+                 polarity_given:int = 1):
         self.sensitivity= sensitivity_given
         self.polarity = polarity_given
         self.dif = [0, 0, 0]
