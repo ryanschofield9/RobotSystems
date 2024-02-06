7e3f0e2a5f3ccfb921d2ebb4cea569213d37d9f3
ryanschofield9 <schofier@oregonstate.edu>
2024-02-06 12:10:58 -0800

    updated picarx_imrpoved to change polarity

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -290,7 +290,7 @@ class Sensor():
 
 class Interpreter():
     def __init__(self, sensitivity_given:float = 0.15, 
-                 polarity_given:int = 1):
+                 polarity_given:int = 0):
         self.sensitivity= sensitivity_given
         self.polarity = polarity_given
         self.dif = [0, 0, 0]
