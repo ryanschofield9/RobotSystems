c9e6dad83d97f545454365a622001caa9e49d67f
ryanschofield9 <schofier@oregonstate.edu>
2024-02-06 13:20:19 -0800

    adding multimodal_control.py

---
+++ picarx/multimodal_control.py
@@ -0,0 +1,40 @@
+from picarx_improved import Picarx, Sensor, Interpreter, Controller
+import rossros as rr 
+import logging
+import time
+
+logging.getLogger().setLevel(logging.INFO)
+
+#initiate buses 
+
+grayscale_sensor = rr.Bus(Sensor.sensor_reading(), "grayscale_bus")
+interpreter_gray = rr.Bus(Interpreter.processing(Sensor.sensor_reading()), "interpreter gray bus") 
+control_grey = rr.Bus(Controller.control_car(Interpreter.processing(Sensor.sensor_reading())), "controller_gray_bus")
+terminate = rr.Bus(0, "Terminate Bus")
+
+produceSignal = rr.Producer(Sensor.sensor_reading(), grayscale_sensor, 0.05, terminate, "Produce grapyscale sensor signal")
+prodConIntGray = rr.ConsumerProducer(Interpreter.processing(), grayscale_sensor, interpreter_gray, 0.1, terminate, "Producer Consumer Interpreter gray scale")
+consumerControl = rr.Consumer(Controller.control_car(), interpreter_gray, 0.1, terminate, "Consumer grayscale controler" )
+
+printBuses = rr.Printer(
+    (grayscale_sensor, interpreter_gray, control_grey, terminate),
+    1,  # delay between printing cycles
+    terminate,  # bus to watch for termination signal
+    "Print bus data",  # Name of printer
+    "Bus readings are: ")  # Prefix for output
+
+terminationTimer = rr.Timer(
+    terminate,  # Output data bus
+    3,  # Duration
+    0.01,  # Delay between checking for termination time
+    terminate,  # Bus to check for termination signal
+    "Termination timer")  # Name of this timer
+
+producer_consumer_list = [produceSignal, 
+                          prodConIntGray, 
+                          consumerControl,
+                          printBuses,
+                          terminationTimer]
+
+# Execute the list of producer-consumers concurrently
+rr.runConcurrently(producer_consumer_list)
